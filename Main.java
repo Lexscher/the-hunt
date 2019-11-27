c784ecc79477a766d95147c65b21f697f556ed96
Alexander Schelchere <ascellie@gmail.com>
2019-11-27 10:45:02 -0500

    Main class

---
+++ Main.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "This is just the beginning.", "Hello! 👋", JOptionPane.PLAIN_MESSAGE);
+    }
+}
